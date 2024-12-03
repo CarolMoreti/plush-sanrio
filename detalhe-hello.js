93a813e7b6ee61b756ad3e84c121d06091b92c40
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2024-12-03 12:47:14 +0000

    .

--- detalhe-hello.js
+++ detalhe-hello.js
@@ -4,15 +4,18 @@ let i2 = document.getElementById('2')
 let i3 = document.getElementById('3')
 let i4 = document.getElementById('4')
 i1.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/hello-kitty/hello-kitty-hero.jpg')
-   /* mainI.innerHTML = '<img src="./assets/img/hello-kitty/hello-kitty-hero.jpg" alt="" class="image-main" id="image">'*/
 })
 i2.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/hello-kitty/hello-kitty2-hero.png')
 })
 i3.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/hello-kitty/hello-kitty.png')
 })
 i4.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/hello-kitty/hello-kitty2.png')
 })
--- detalhes-kuromi.js
+++ detalhes-kuromi.js
@@ -4,14 +4,18 @@ let i2 = document.getElementById('2')
 let i3 = document.getElementById('3')
 let i4 = document.getElementById('4')
 i1.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/kuromi/kuromi-hero2.png')
 })
 i2.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/kuromi/kuromi-hero.jpg')
 })
 i3.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/kuromi/kuromi.png')
 })
 i4.addEventListener("click", function(e){
+    e.preventDefault();
     mainI.setAttribute('src','./assets/img/kuromi/kuromi2.png')
 })
